b5cefc78c797fc41d161fc5129d196b1668eba87
Xuchzh <827884896@qq.com>
2020-05-12 10:24:05 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import './styles/index.less'
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
